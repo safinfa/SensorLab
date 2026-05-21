7603401d734dab6ddf3e037c4cef73cf6980a295
safinfa <Safinfarrashidiq@gmail.com>
2026-05-22 03:45:44 +0700

    move Firebase credentials to environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvmjHaixGvd1JcR5pKRrp1WMIT7HQQ5Xc",
-  authDomain: "stemm-lab-6535f.firebaseapp.com",
-  projectId: "stemm-lab-6535f",
-  storageBucket: "stemm-lab-6535f.firebasestorage.app",
-  messagingSenderId: "374386966535",
-  appId: "1:374386966535:web:6f057b71805b6b35e12aa2",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
