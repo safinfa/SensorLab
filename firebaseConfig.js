51fc290a80bc28d2fdd7dd160bd7dd58c4122b32
safinfa <Safinfarrashidiq@gmail.com>
2026-06-05 08:44:54 +0700

    fix: hardcode firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyAvmjHaixGvd1JcR5pKRrp1WMIT7HQQ5Xc",
+  authDomain: "stemm-lab-6535f.firebaseapp.com",
+  projectId: "stemm-lab-6535f",
+  storageBucket: "stemm-lab-6535f.firebasestorage.app",
+  messagingSenderId: "374386966535",
+  appId: "1:374386966535:web:6f057b71805b6b35e12aa2",
 };
 
 const app = initializeApp(firebaseConfig);
